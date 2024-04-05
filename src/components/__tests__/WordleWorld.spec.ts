400861acf207d8cd6cd52d853e4200f535bec7e0
mingmoth <mingmoth@gmail.com>
2024-04-05 12:16:55 +0800

    chore: remove clutter - rename HelloWorld to WordleWorld

--- src/components/__tests__/WordleWorld.spec.ts
+++ src/components/__tests__/WordleWorld.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
+import WordleWorld from '../WordleWorld.vue'
 
-describe('HelloWorld', () => {
+describe('WordleWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(WordleWorld, { props: { msg: 'Hello Vitest' } })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
