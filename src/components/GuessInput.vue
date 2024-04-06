3f4e049a12bfdd1b1c0c0d45b87f3e146c87d1b7
mingmoth <mingmoth@gmail.com>
2024-04-06 10:41:04 +0800

    feat: add styles

--- src/components/GuessInput.vue
+++ src/components/GuessInput.vue
@@ -31,5 +31,48 @@ function onSubmit() {
         autofocus
         @blur="({target}) => (target as HTMLInputElement).focus()"
         @keydown.enter="onSubmit"
-    >
+    />
+    <ul class="word">
+        <li v-for="(letter, index) in guessInProgress.padEnd(WORD_SIZE, ' ')"
+            :key="`${letter}-${index}`"
+            :data-letter="letter"
+            class="letter"
+            v-text="letter"
+        />
+    </ul>
 </template>
+
+<style scoped>
+/* input {
+    position: absolute;
+    opacity: 0;
+} */
+.word {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    gap: 0.25rem;
+}
+.letter {
+    background-color: white;
+    border: 1px solid hsl(0, 0%, 70%);
+    width: 5rem;
+    height: 5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 2rem;
+    font-weight: bolder;
+}
+li:not([data-letter=" "]) {
+    animation: pop 100ms;
+}
+@keyframes pop {
+    0% {
+        transform: scale(1);
+    }
+    50% {
+        transform: scale(1.4);
+    }
+}
+</style>
--- src/components/WordleWorld.vue
+++ src/components/WordleWorld.vue
@@ -15,9 +15,36 @@ const guessSubmitted = ref('');
 </script>
 
 <template>
-    <GuessInput @guess-submit="guessSubmitted = $event" />
-    <p
-        v-if="guessSubmitted.length > 0"
-        v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
-    />
+    <main>
+        <GuessInput @guess-submit="guessSubmitted = $event" />
+        <p
+            v-if="guessSubmitted.length > 0"
+            v-text="guessSubmitted === wordOfTheDay ? VICTORY_MESSAGE : DEFEAT_MESSAGE"
+        />
+    </main>
 </template>
+
+<style scoped>
+main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 3rem;
+}
+.end-of-game-message {
+    font-size: 3rem;
+    animation: end-of-game-message-animation 700ms forwards;
+    white-space: nowrap;
+    text-align: center;
+}
+@keyframes end-of-game-message-animation {
+    0% {
+        opacity: 0;
+        transform: rotateZ(0);
+    }
+    100% {
+        opacity: 1;
+        transform: translateY(2rem);
+    }
+}
+</style>
